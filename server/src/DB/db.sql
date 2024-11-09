4968317815783c6901275e9debade2e00aa841c9
UnyQ-ahm-laptop <farid.falaha05@gmail.com>
2024-11-09 14:41:42 +0100

    Final

--- server/src/DB/db.sql
+++ server/src/DB/db.sql
@@ -37,3 +37,22 @@ CREATE TABLE item (
 	reporter int REFERENCES "user"(id),
 	collector int REFERENCES "user"(id)
 );
+
+
+INSERT INTO "user" ("name", surname, email, "password", point)
+VALUES
+    ('Chris Adams', 'Adams', 'chrisadams@example.com', 'hashed_password', 85),
+    ('Rachel Green', 'Green', 'rachelgreen@example.com', 'hashed_password', 90),
+    ('Tom Harris', 'Harris', 'tomharris@example.com', 'hashed_password', 110),
+    ('Sarah Taylor', 'Taylor', 'sarahtaylor@example.com', 'hashed_password', 130),
+    ('Liam Williams', 'Williams', 'liamwilliams@example.com', 'hashed_password', 70),
+    ('Olivia Martinez', 'Martinez', 'oliviamartinez@example.com', 'hashed_password', 95),
+    ('James Wilson', 'Wilson', 'jameswilson@example.com', 'hashed_password', 160),
+    ('Sophia Davis', 'Davis', 'sophiadavis@example.com', 'hashed_password', 80),
+    ('Benjamin Moore', 'Moore', 'benjaminmoore@example.com', 'hashed_password', 40),
+    ('Mia Scott', 'Scott', 'miascott@example.com', 'hashed_password', 65),
+    ('Alexander Clark', 'Clark', 'alexanderclark@example.com', 'hashed_password', 150),
+    ('Ella Walker', 'Walker', 'ellawalker@example.com', 'hashed_password', 135),
+    ('Jackson Allen', 'Allen', 'jacksonallen@example.com', 'hashed_password', 100),
+    ('Amelia Young', 'Young', 'ameliayoung@example.com', 'hashed_password', 125),
+    ('Ethan King', 'King', 'ethanking@example.com', 'hashed_password', 55);
